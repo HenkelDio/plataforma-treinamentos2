38f9093ca3f4c995fe12d66a96795342b64d993f
Samuel Diel <emanuel.henkel@gmail.com>
2022-11-22 21:21:04 -0300

    Criação de treinamentos

--- client/src/pages/pdfUplader/index.jsx
+++ client/src/pages/pdfUplader/index.jsx
@@ -1,18 +1,26 @@
 
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import Axios from "axios"
 
 export default function PdfUploader() {
 
+    let [file, setFile] = useState();
+
+    const uploadFiles = () => {
+        let formData = new FormData();
+        formData.append("courseFile", file, file.name)
+
+        Axios.post("http://localhost:3001/createCourse", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        })
+    }
+
     return (
         <div>
-            <form ref={useRef('uploadForm')}
-                id='uploadForm'
-                action='http://localhost:3001/fileUploader'
-                method='post'
-                encType="multipart/form-data">
-                <input type="file" name="uploadedFile" />
-                <input type='submit' value='Upload!' />
-            </form>
+            <input type="file" onChange={e => setFile(e.target.files[0])} />
+            <button onClick={uploadFiles}>Upload</button>
         </div>
     )
 }
