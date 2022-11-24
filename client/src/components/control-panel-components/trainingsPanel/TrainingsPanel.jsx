c9bf25b1162b521a62ce7b2350ab0c92a7739ddb
Samuel Diel <emanuel.henkel@gmail.com>
2022-11-23 22:49:16 -0300

    att

--- client/src/components/control-panel-components/trainingsPanel/TrainingsPanel.jsx
+++ client/src/components/control-panel-components/trainingsPanel/TrainingsPanel.jsx
@@ -18,7 +18,7 @@ export default function TrainingsPanel(props){
             })
         }
         getCourse()
-    }, [listTraining])
+    }, [])
 
     const closeModal = () =>{
         setIsOpen(false)
