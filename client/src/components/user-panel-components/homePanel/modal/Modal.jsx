f1d003af62563e418be6bb1220a20eb11a60c9e4
HenkelDio <willianhenkel@gmail.com>
2022-11-23 23:23:13 -0300

    att

--- client/src/components/user-panel-components/homePanel/modal/Modal.jsx
+++ client/src/components/user-panel-components/homePanel/modal/Modal.jsx
@@ -10,19 +10,25 @@ export default function ModalTraining(props){
     overlayClassName={styles.modalOverlay}
     className={styles.modal}
     ariaHideApp={false}>
-      <div className={styles.headerModal}>
-        <h1>NR 20: Combate contra o Incêndio</h1>
-      </div>
-      <div className={styles.bodyModal}>
-        <div className={styles.pdf}>
-          aqui vai o pdf
+      <div className={styles.modalTraining}>
+        <div className={styles.headerModal}>
+          <h1>NR 20: Combate contra o Incêndio</h1>
         </div>
-        <div className={styles.content}>
-          aqui vai o conteudo
-        </div>
-        <div className={styles.features}>
-          <button>ir para a prova</button>
-          <button>Fechar</button>
+        <div className={styles.bodyModal}>
+          <div className={styles.pdf}>
+            aqui vai o pdf
+          </div>
+          <div className={styles.content}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nemo quos praesentium cupiditate eligendi minima ab expedita aperiam reprehenderit corporis ullam nesciunt? Quam provident explicabo earum laborum ipsa alias asperiores.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nemo quos praesentium cupiditate eligendi minima ab expedita aperiam reprehenderit corporis ullam nesciunt? Quam provident explicabo earum laborum ipsa alias asperiores.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nemo quos praesentium cupiditate eligendi minima ab expedita aperiam reprehenderit corporis ullam nesciunt? Quam provident explicabo earum laborum ipsa alias asperiores.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nemo quos praesentium cupiditate eligendi minima ab expedita aperiam reprehenderit corporis ullam nesciunt? Quam provident explicabo earum laborum ipsa alias asperiores.
+            </p>
+          </div>
+          <div className={styles.features}>
+            <button>ir para a prova</button>
+            <button>Fechar</button>
+          </div>
         </div>
       </div>
     </Modal>
