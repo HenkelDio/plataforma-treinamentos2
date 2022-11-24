a61be5b89d469f8cf987bd5259c64a91ecef19bd
HenkelDio <willianhenkel@gmail.com>
2022-11-23 22:48:56 -0300

    att

--- client/src/components/user-panel-components/homePanel/modal/Modal.jsx
+++ client/src/components/user-panel-components/homePanel/modal/Modal.jsx
@@ -1,11 +1,11 @@
 import Modal from 'react-modal';
 import styles from "./modal.module.css";
 
-export default function ModalTraining(){
+export default function ModalTraining(props){
   return(
     <Modal 
     isOpen={props.openModal}
-    onRequestClose={closeModal}
+    onRequestClose={props.closeModal}
     contentLabel="Exemplo"
     overlayClassName={styles.modalOverlay}
     className={styles.modal}
