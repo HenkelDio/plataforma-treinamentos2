26e6afe290a12c1ca213a30002145c6dcc58e8a4
HenkelDio <willianhenkel@gmail.com>
2022-11-30 22:06:49 -0300

    att

--- client/src/components/user-panel-components/homePanel/modal/pdf/FramePdf.jsx
+++ client/src/components/user-panel-components/homePanel/modal/pdf/FramePdf.jsx
@@ -3,7 +3,7 @@ import sample from "./firstlook.pdf"
 export default function FramePdf() {
   return(
     <>
-      <iframe data={sample} type="application/pdf" width="100%" height="100%">
+      <iframe src={sample} type="application/pdf" width="100%" height="100%">
       </iframe>
     </>
   )
