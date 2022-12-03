0a4d02985ea514f0155ef34b788612995a543fbe
Samuel Diel <emanuel.henkel@gmail.com>
2022-12-03 19:41:49 -0300

    att

--- client/src/components/user-panel-components/homePanel/modal/pdf/FramePdf.jsx
+++ client/src/components/user-panel-components/homePanel/modal/pdf/FramePdf.jsx
@@ -16,7 +16,7 @@ export default function FramePdf(props) {
   }, []);
 
   return( pdfInfo ? 
-    <>
+    <> 
       <iframe src={`https://souzatreinamentosst.com.br:4000/${pdfInfo.courseDir}/${pdfInfo.coursePdf}`} type="application/pdf" width="100%" height="100%" />
     </> : <></>
   )
