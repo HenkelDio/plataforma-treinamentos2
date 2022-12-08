2880e104d6e1107c52737b564862e2db4fd2a781
HenkelDio <willianhenkel@gmail.com>
2022-12-07 23:52:35 -0300

    att

--- client/src/components/user-panel-components/homePanel/modal/questions/modalQuestions.jsx
+++ client/src/components/user-panel-components/homePanel/modal/questions/modalQuestions.jsx
@@ -1,8 +1,5 @@
 import styles from "./modalQuestions.module.css";
 import Modal from "react-modal";
-import questions from "./questions.js"
-import InputBoxQuestion from "./inputBoxQuestion";
-
 export default function ModalQuestion(props){
 
   return(
@@ -16,20 +13,6 @@ export default function ModalQuestion(props){
     ariaHideApp={false}>
     <div className={styles.headerModal}>
       <h1>Questões de {props.data.data.data.course_title}</h1>
-    </div>
-    <div className={styles.bodyModal}>
-      <p>{questions[0].question}</p>
-      <div className={styles.inputBox}>
-        {questions[0].options.map((val)=>{
-          return <InputBoxQuestion data={val}/>
-        })}
-      </div>
-      <p>{questions[1].question}</p>
-      <div className={styles.inputBox}>
-        {questions[1].options.map((val)=>{
-          return <InputBoxQuestion data={val}/>
-        })}
-      </div>
     </div>
     </Modal>
     </>
