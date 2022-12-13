c885a9b03ac371df48918288a67ff6666c7f476f
HenkelDio <willianhenkel@gmail.com>
2022-12-12 23:40:48 -0300

    att

--- client/src/pages/landingPage/index.jsx
+++ client/src/pages/landingPage/index.jsx
@@ -1,11 +1,26 @@
 import styles from './index.module.css';
 import logo from '../../assets/logo.png'
 
+window.onscroll = function() {onscrollHeader()}
+
+function onscrollHeader(){
+  if(document.documentElement.scrollTop > 600){
+    let innerHeader = document.getElementById("innerHeader")
+    innerHeader.style.position = "fixed"
+    innerHeader.style.backgroundColor = "rgb(63, 63, 63)"
+  } else {
+    let innerHeader = document.getElementById("innerHeader")
+    innerHeader.style.position = "sticky"
+    innerHeader.style.backgroundColor = "transparent"
+  }
+}
+  
+
 export default function LandingPage(){
   return(
     <div className={styles.LandingPage}>
       <div className={styles.header}>
-        <div className={styles.innerHeader}>
+        <div id="innerHeader" className={styles.innerHeader}>
           <div className={styles.logo}>
             <img src={logo}></img>
           </div>
@@ -24,6 +39,22 @@ export default function LandingPage(){
             <button>ACESSAR</button>
           </div>
         </div>
+      </div>
+      <div className={styles.bodyPage}>
+        <section className={styles.about}>
+          <div className={styles.title}>
+            <h2>Sobre</h2>
+          </div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore distinctio atque numquam temporibus odio corrupti sed qui, praesentium ipsum, rerum nobis quisquam repudiandae voluptatum doloribus consectetur, molestias perferendis rem. Temporibus.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore distinctio atque numquam temporibus odio corrupti sed qui, praesentium ipsum, rerum nobis quisquam repudiandae voluptatum doloribus consectetur, molestias perferendis rem. Temporibus.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore distinctio atque numquam temporibus odio corrupti sed qui, praesentium ipsum, rerum nobis quisquam repudiandae voluptatum doloribus consectetur, molestias perferendis rem. Temporibus.
+            </p>
+        </section>
       </div>
     </div>
   )
