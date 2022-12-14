43f190452d02353de06cc91fc5ceb8956bfdc954
Manullx <emanuel.henkel@gmail.com>
2022-12-13 22:16:29 -0300

    att

--- server/clearCoursesDir.js
+++ server/clearCoursesDir.js
@@ -1,9 +1,9 @@
 
 const fs = require("fs");
-
+fs.ra
 for (let course of fs.readdirSync("./treinamentos")) {
     for (let file of fs.readdirSync("./treinamentos/" + course)) {
         fs.unlinkSync("./treinamentos/" + course + "/" + file)
     }
-    fs.rmDirSync("./treinamentos/" + course)
+    fs.rmdirSync("./treinamentos/" + course)
 }
