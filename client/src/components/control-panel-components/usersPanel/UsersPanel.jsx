f5b4a5519f88ee5b804c4f22a9ef73b1ea087751
Manullx <emanuel.henkel@gmail.com>
2022-12-19 22:22:35 -0300

    Busca de usuários

--- client/src/components/control-panel-components/usersPanel/UsersPanel.jsx
+++ client/src/components/control-panel-components/usersPanel/UsersPanel.jsx
@@ -4,48 +4,47 @@ import Admin from "./usersSelect/admin/admin"
 import User from "./usersSelect/user/user"
 import Company from "./usersSelect/company/company"
 import { useState, useEffect } from "react"
-import {IoIosAdd} from 'react-icons/io'
+import { IoIosAdd } from 'react-icons/io'
 import Axios from "axios"
 
 
-export default function UsersPanel(props){
+export default function UsersPanel(props) {
     const [modalIsOpen, setIsOpen] = useState(false);
     const [userType, setUserType] = useState("");
     const [listUsers, setListUsers] = useState([]);
-    const [search, setSearch] = useState('')
+    const [searchCompany, setSearchCompany] = useState("");
+    const [searchUser, setSearchUser] = useState("");
 
-    const openModal = () =>{
+    const openModal = () => {
         setIsOpen(true)
     }
 
-    const closeModal = () =>{
+    const closeModal = () => {
         setIsOpen(false)
     }
 
-    const users = [];
-    
     useEffect(_ => {
         const getUsers = async _ => {
             let route = `${require("../../../defaultRoute")}/getUsers/${userType}`
             await Axios.get(route).then(res => {
                 if (res) {
                     setListUsers(res.data)
-                    
-                    for(let i = 0; i < res.data.length; i++){
-                        users.push(res.data[i].admin_name)
-                    }
-
-                    console.log(users);
-                
                 }
             })
         }
         getUsers()
     }, [userType])
 
-    const usersFiltered = users.filter((user) => user.startWith(search));
+    function search() {
+        const route = `${require("../../../defaultRoute")}/${userType}/${(userType === "Users" ? searchUser : searchCompany)}`
+        Axios.get(route).then(res => {
+            if (res) {
+                console.log(res)
+            }
+        })
+    }
 
-    return(
+    return (
         <div className={styles.UsersPanel}>
 
             <ModalAddUser openModal={modalIsOpen} closeModal={closeModal} />
@@ -56,14 +55,14 @@ export default function UsersPanel(props){
             </div>
             <div className={styles.bodyUsers}>
                 <div onClick={openModal} className={styles.addUserContainer}>
-                    <p className={styles.addIcon}><IoIosAdd/></p>
+                    <p className={styles.addIcon}><IoIosAdd /></p>
                     <p>Adicionar novo usuário</p>
                 </div>
             </div>
             <div className={styles.listUsers}>
                 <p>Selecione o Usuário</p>
                 <div className={styles.selectTypeAccount}>
-                    <select onChange={e=>setUserType(e.target.value)}>
+                    <select onChange={e => setUserType(e.target.value)}>
                         <option disabled selected="selected">Selecione o Tipo</option>
                         <option value="Admins">Administrador</option>
                         <option value="Companies">Empresa</option>
@@ -71,42 +70,53 @@ export default function UsersPanel(props){
                     </select>
                 </div>
                 <div className={styles.list}>
-                    <input 
-                    type='text' 
-                    placeholder="Procurar"
-                    value={search}
-                    onChange={e => setSearch(e.target.value)}
-                    ></input>
                     {
-                        (userType === "Admins") && listUsers.map((val)=>{
-                            return <Admin 
-                            id={val.admin_id}
-                            name={val.admin_name}
-                            email={val.admin_email}
+                        (
+                            userType === "Users" ? (
+                                <>
+                                    <input type="text" placeholder="CPF do Usuário" onChange={e => {setSearchUser(e.target.value)}} />
+                                    <input type="button" value="buscar" onClick={search} />
+                                </>
+                            ) : (
+                                userType === "Companies" ? (
+                                    <>
+                                        <input type="text" placeholder="CNPJ da Empresa" onChange={e => {setSearchCompany(e.target.value)}} />
+                                        <input type="button" value="buscar" onClick={search} />
+                                    </>
+                                ) : (<></>)
+                            )
+                        )
+                    }
+                    {
+                        (userType === "Admins") && listUsers.map((val) => {
+                            return <Admin
+                                id={val.admin_id}
+                                name={val.admin_name}
+                                email={val.admin_email}
                             />
                         })
                     }
                     {
-                        (userType === "Users") && listUsers.map((val)=>{
+                        (userType === "Users") && listUsers.map((val) => {
                             return <User
-                            id={val.user_id}
-                            name={val.user_name}
-                            email={val.user_email}
-                            register={val.user_register}
-                            phone={val.user_telephone}
-                            id_company={val.user_company_id}
+                                id={val.user_id}
+                                name={val.user_name}
+                                email={val.user_email}
+                                register={val.user_register}
+                                phone={val.user_telephone}
+                                id_company={val.user_company_id}
                             />
                         })
                     }
                     {
-                        (userType === "Companies") && listUsers.map((val)=>{
-                            return <Company 
-                            id={val.company_id}
-                            name={val.company_name}
-                            email={val.company_email}
-                            contact={val.company_contact}
-                            register={val.company_register}
-                            phone={val.company_telephone}
+                        (userType === "Companies") && listUsers.map((val) => {
+                            return <Company
+                                id={val.company_id}
+                                name={val.company_name}
+                                email={val.company_email}
+                                contact={val.company_contact}
+                                register={val.company_register}
+                                phone={val.company_telephone}
                             />
                         })
                     }
