894ca76c68f121a0db0126be999d1c3d15ceb262
HenkelDio <willianhenkel@gmail.com>
2022-12-21 22:27:43 -0300

    Busca de Usuario

--- client/src/components/control-panel-components/usersPanel/UsersPanel.jsx
+++ client/src/components/control-panel-components/usersPanel/UsersPanel.jsx
@@ -72,12 +72,23 @@ export default function UsersPanel(props) {
                     </select>
                 </div>
                 <div className={styles.list}>
-                    <input 
-                    type='text' 
-                    placeholder="Procurar"
-                    value={search}
-                    onChange={e => setSearch(e.target.value)}
-                    ></input>
+                    {
+                        (
+                            userType === "Users" ? (
+                                <>
+                                    <input type="text" placeholder="CPF do Usuário" onChange={e => {setSearchUser(e.target.value)}} />
+                                    <input type="button" value="buscar" onClick={search} />
+                                </>
+                            ) : (
+                                userType === "Companies" ? (
+                                    <>
+                                        <input type="text" placeholder="CNPJ da Empresa" onChange={e => {setSearchCompany(e.target.value)}} />
+                                        <input type="button" value="buscar" onClick={search} />
+                                    </>
+                                ) : (<></>)
+                            )
+                        )
+                    }
                     {
                         (userType === "Admins") && listUsers.map((val) => {
                             return <Admin
