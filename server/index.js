2131e5c063b28f2ebb00de5bfea5869153157c5d
Emanuel Henkel Gonçalves <emanuel.henkel@gmail.com>
2023-01-15 00:38:08 -0300

    att

--- server/index.js
+++ server/index.js
@@ -85,7 +85,7 @@ app.post("/loginPasswordUser", async (req, res) => {
     const userInfo = {
         permission: (userDB === "Admins" ? "admin" : (userDB === "Companies" ? "company" : "user")),
         name: user.dataValues[(userDB === "Admins" ? "admin_name" : (userDB === "Companies" ? "company_name" : "user_name"))],
-        id: user.dataValues[(userDB === "Admins" ? "admin_id" : (userDB === "Companies" ? "company_id" : "user_id"))].Admins,
+        id: user.dataValues[(userDB === "Admins" ? "admin_id" : (userDB === "Companies" ? "company_id" : "user_id"))],
         email: user.dataValues[(userDB === "Admins" ? "admin_email" : (userDB === "Companies" ? "company_email" : "user_email"))]
     }
     res.send({ authorized: user.dataValues[passwordCol] === userPassword, userInfo })
