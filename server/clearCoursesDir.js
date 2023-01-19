7bec8e28dd964270e31921e088293a0f95e1d2dc
Emanuel Henkel Gonçalves <emanuel.henkel@gmail.com>
2023-01-18 21:21:47 -0300

    clear corses dir

--- server/clearCoursesDir.js
+++ server/clearCoursesDir.js
@@ -1,6 +1,6 @@
 
 const fs = require("fs");
-fs.ra
+
 for (let course of fs.readdirSync("./treinamentos")) {
     for (let file of fs.readdirSync("./treinamentos/" + course)) {
         fs.unlinkSync("./treinamentos/" + course + "/" + file)
